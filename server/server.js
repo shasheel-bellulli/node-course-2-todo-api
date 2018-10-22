ab522be90a0b179bcfcb0f733314c1d301a28210
Shasheel BELLULLI <shasheel.bellulli-cic@ibm.com>
2018-10-22 16:28:48 +0200

    Add Post /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var {mongoose} = require('./db/mongoose.js')
+var {Todo} = require('./models/Todo')
+var {User} = require('./models/User')
+
+var app = express()
+app.use(bodyParser.json())
+app.post('/todos', (req, res) => {
+var todo = new Todo({
+    text: req.body.text
+})
+
+todo.save().then((doc) => {
+    res.send(doc)
+}, (e) => {
+    res.status(400).send(e)
+})
+console.log(req.body)
+})
+
+app.listen(3000,() => {
+    console.log('Started on port 3000')
+})
