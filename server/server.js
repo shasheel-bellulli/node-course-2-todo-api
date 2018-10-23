95b7a26d3585cfb4d73a517d849740c3283da735
Shasheel BELLULLI <shasheel.bellulli-cic@ibm.com>
2018-10-23 12:05:15 +0200

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -20,6 +20,15 @@ todo.save().then((doc) => {
 console.log(req.body)
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }), (e) => {
+        res.status(400).send(e)
+    }
+})
 app.listen(3000,() => {
     console.log('Started on port 3000')
 })
+
+module.exports = {app}
