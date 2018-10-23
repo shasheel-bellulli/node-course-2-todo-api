b9df3a9aec800f58e2d4eb717d41c020d29e6b05
Shasheel BELLULLI <shasheel.bellulli-cic@ibm.com>
2018-10-23 16:22:38 +0200

    Add test cases for /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,12 @@ const request = require('supertest')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/Todo')
-
+const {ObjectID} = require('mongodb')
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test todo'
 }, {
+    _id: new ObjectID('5bcf2becc3ac091bd4ac5d07'),
     text: 'second test todo'
 }]
 
@@ -68,4 +70,32 @@ describe('GET /todos', () => {
         })
         .end(done)
     })
+})
+
+describe('GET /todos:id', () => {
+    it('should return todos doc ', (done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text)
+        }).end(done)
+    })
+
+    it('should return 404 if todo not found ', (done) => {
+        var hexId = new ObjectID().toHexString()
+        request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404)
+        .end(done)
+    })
+
+    it('should return 404 for non', (done) => {
+        request(app)
+        .get(`/todos/1234`)
+        .expect(404)
+        .end(done)
+    })
+
+
 })
