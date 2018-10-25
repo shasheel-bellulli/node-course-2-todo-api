4abf1cef5be22db0c09b18305605ce3d136e8e7d
Shasheel BELLULLI <shasheel.bellulli-cic@ibm.com>
2018-10-25 15:42:50 +0200

    Add post users route and token methode

--- server/server.js
+++ server/server.js
@@ -1,12 +1,4 @@
-var env = process.env.NODE_ENV || 'development'
-console.log('env *****', env)
-if(env === 'development'){
-process.env.PORT = 3000
-process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test'){
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-}
+require('./config/config.js')
 const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -99,6 +91,18 @@ app.patch('/todos/:id', (req, res) => {
         res.status(400).send()
     })
 
+})
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password'])
+    var user = new User(body)
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then((token) => {
+        res.header('x-auth', token).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
 })
 
 app.listen(port,() => {
